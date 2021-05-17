120637c0606e9cbf3e05e622740366215fb1e7d0
13tinydots <13tinydots@users.noreply.github.com>
2021-05-17 10:50:18 -0500

    add concatenation

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
-# passwordgenerator
+initial commit 05/17/2021 
+
+created "logic/js-file" branch for logic work 05/17/2021 8:20am
+
+added concatenation code to body for assembly of strings to pick from 5/17/2021 10:41am
--- script.js
+++ script.js
@@ -10,5 +10,45 @@ function writePassword() {
 
 }
 
+
+
+var numberOfCharacters = 8;
+var lowerCaseRequest = "true";
+var upperCaseRequest = "true";
+var specialCharRequest = "true";
+var charset = [""];
+
+function generatePassword() {
+  const digits = ["1","2","3","4","5","6","7","8","9","0"];
+  const spCharacters = [",","!","#","$","%","&","'","*","+","-",".","/",":",";","<",">","=","?","@"];
+  const alphabetLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const alphabetUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  
+
+  if (lowerCaseRequest = "true") {
+      charset.concat (alphabetLowerCase);
+      
+      } 
+
+  if (upperCaseRequest = "true") {
+      charset.concat (alphabetUpperCase);
+      } 
+  
+  if (specialCharRequest = "true") {
+      charset.concat (spCharacters);
+      } 
+    console.log(charset);
+  }
+ 
+  generatePassword();
+
+
+
+ 
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
+
