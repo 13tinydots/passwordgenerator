d8ad7bd217112923094cdda3dd928db6cc1f3ecb
13tinydots <13tinydots@users.noreply.github.com>
2021-05-18 08:37:42 -0500

    comment JS file and built array target engine

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ created "logic/js-file" branch for logic work 05/17/2021 8:20am
 added concatenation code to body for assembly of strings to pick from 5/17/2021 10:41am
 
 fixed non-working .concat method implementation 8:00a 05/18/2021 
+
+commented working points in the file and built array target engine. 05/18/2021 8:36a
--- script.js
+++ script.js
@@ -9,42 +9,73 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+//need to create user input
+// setting global variables to execute outside of button click so as to not repeat 
+// concatenation on the target array more than once
 var charset = [];
 var numberOfCharacters = 8;
-var digitsRequested = true;
+var digitsRequest = true;
 var lowerCaseRequest = true;
 var upperCaseRequest = true;
 var specialCharRequest = true;
 
+// these arrays are used to build a target from which a character to add to a new password array
+// which will later be turned into a string for the user once the iteration process is complete
 var digits = ["1","2","3","4","5","6","7","8","9","0"];
 var spCharacters = [",","!","#","$","%","&","'","*","+","-",".","/",":",";","<",">","=","?","@"];
 var alphabetLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var alphabetUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  
-if (digitsRequested = true){
+ 
+// build the charset array from which the password is randomly selected
+if (digitsRequest = true){
   charset = digits.concat(charset);
-  console.log(charset);
 }
 
 if (lowerCaseRequest = true) {
   charset = alphabetLowerCase.concat(charset);
-  console.log(charset);
 } 
 
 if (upperCaseRequest = true) {
   charset = alphabetUpperCase.concat(charset);
-  console.log(charset);
 } 
   
 if (specialCharRequest = true) {
   charset = spCharacters.concat(charset);
-  console.log(charset);
 } 
 
+// iteration process for generating the password array
 function generatePassword() {
-for(var i = 0; i <= numberOfCharacters; i++){
 
+// validates input as containing at least one set of target characters for the password
+  if(digitsRequest || lowerCaseRequest || upperCaseRequest || specialCharRequest){
+
+// validated input proceeds to the iteration process
+      
+      function chooseArrayValue() { 
+        for(var i = 0; i <= numberOfCharacters; i++){
+        var length = (charset.length);
+        var target = 0;
+        console.log(charset.length);
+
+          function generateTarget () {
+          if (length < target = Math.floor((Math.random() * 100) + 1)){
+          generateTarget();}
+          else {
+            
+          } 
+          }
+          }
+        }
+        
+
+
+
+
+
+    } else {
+      console.log("You must select one type of character for your password")
+      return;
+    }
   }
 }
 
